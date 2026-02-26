4f563e3fbef11de4ced36cb0ad4493b8348acbd5
chintu kumar <chintudas499@gmail.com>
2026-02-26 14:56:24 +0530

    cart ui design

---
+++ app/src/main/java/com/example/freshyzoappmodule/ui/Fragments/AccountFragment.kt
@@ -0,0 +1,30 @@
+package com.example.freshyzoappmodule.ui.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.freshyzoappmodule.R
+
+
+class AccountFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account2, container, false)
+
+        // todo ====================
+    }
+
+
+}
